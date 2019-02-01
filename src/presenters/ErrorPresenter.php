00f383e307431cbed665b6ec0ab1d95252c591cc
Jiri Spacek <jiri@jirispacek.info>
2019-02-01 14:35:22 +0100

    fixes

--- src/presenters/ErrorPresenter.php
+++ src/presenters/ErrorPresenter.php
@@ -3,28 +3,12 @@
 use Nette\Diagnostics\Debugger;
 
 
-
 /**
  * Error presenter.
  */
-class ErrorPresenter extends BasePresenter
+class ErrorPresenter extends DBCBasePresenter
 {
-
-    public function startup()
-    {
-
-		parent::startup();
-
-		$this->lang = $this->_getLanguage();
-
-		if(!in_array($this->lang, array_keys($this->enabledLanguages), true)) {
-			$this->lang = "en_US";
-		}
-
-		$this->template->language = $this->lang;
-    }
-
-    /**
+	/**
 	 * @param  Exception
 	 * @return void
 	 */
@@ -37,28 +21,10 @@ class ErrorPresenter extends BasePresenter
 		} elseif ($exception instanceof Nette\Application\BadRequestException) {
 			$code = $exception->getCode();
 
-			// get path
-
-            $redirect = $this->context->getService("redirectRepository")->find()->where("url = ?", $_SERVER["REQUEST_URI"])->where("lang = ?", $this->lang)->fetch();
-            if($redirect) {
-                $this->redirectUrl($redirect->new, 301); exit;
-            }
-
-
 			// load template 403.latte or 404.latte or ... 4xx.latte
-			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
-
-			if(!in_array($this->lang, $this->enabledLanguages, true)) {
-			    $this->lang = "en_US";
-            }
-
-			// log to access.log
-			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
-
+			$this->setView(\in_array($code, array(403, 404, 405, 410, 500), true) ? $code : '4xx');
 		} else {
 			$this->setView('500'); // load template 500.latte
-			Debugger::log($exception, Debugger::ERROR); // and log exception
 		}
 	}
-
 }
