3584227c6d40acf9adeb017ee0228d2c93e9ed6d
Jiri Spacek <jiri@jirispacek.info>
2019-02-01 14:37:24 +0100

    fixes

--- src/presenters/ErrorPresenter.php
+++ src/presenters/ErrorPresenter.php
@@ -1,24 +1,14 @@
 <?php
 
-use Nette\Diagnostics\Debugger;
-
-
-/**
- * Error presenter.
- */
-class ErrorPresenter extends DBCBasePresenter
+class ErrorPresenter extends BasePresenter
 {
-	/**
-	 * @param  Exception
-	 * @return void
-	 */
 	public function renderDefault($exception)
 	{
 		if ($this->isAjax()) { // AJAX request? Just note this error in payload.
 			$this->payload->error = TRUE;
 			$this->terminate();
 
-		} elseif ($exception instanceof Nette\Application\BadRequestException) {
+		} elseif ($exception instanceof \Nette\Application\BadRequestException) {
 			$code = $exception->getCode();
 
 			// load template 403.latte or 404.latte or ... 4xx.latte
