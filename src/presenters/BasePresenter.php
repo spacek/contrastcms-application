eff44c842a365371b6f0ce3be3329fda090b73a3
Jiri Spacek <jiri@jirispacek.info>
2019-04-18 09:23:13 +0200

    fix

--- src/presenters/BasePresenter.php
+++ src/presenters/BasePresenter.php
@@ -34,8 +34,7 @@ abstract class BasePresenter extends UI\Presenter
 			$this->enabledLanguages['en_US'] = 'English';
 		}
 
-		$this->lang = "cs_CZ";
-		$this->template->language = $this->lang;
+		$this->language = $this->lang = $this->getParameter("lang", 'cs_CZ');
 
 		// Load default variables / options
 		$options = $this->context->getService("optionRepository");
